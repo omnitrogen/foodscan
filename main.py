3b393f70bdd32c444abfb838d9795eb6f6bbe4d1
Félix Defrance <felixgif@gmail.com>
2018-06-11 00:30:21 +0200

    detect barcode, decode it and print infos about product :+1:

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyzbar import pyzbar
 import numpy as np
 import cv2
 import requests
+from pprint import pprint
 
 
 cap = cv2.VideoCapture(0)
@@ -17,25 +18,36 @@ while True:
             n = len(points)
             for j in range(n):
                 cv2.line(frame, points[j], points[(j+1) % n], (255, 0, 0), 3)
+
         resp = requests.get("https://fr.openfoodfacts.org/api/v0/produit/"
                             + decode[0].data.decode()
                             + ".json")
+
         if resp.json()["status"] == 1:
-            product = resp.json()["product"]["product_name"]
+            product = resp.json()["product"]["brands"]
         elif resp.json()["status"] == 0:
             product = "product not in db"
-        else:
-            product = "no internet connection"
 
         cv2.putText(frame,
-                    decode[0].type.decode() + ", "
+                    decode[0].type + ", "
                     + decode[0].data.decode() + ", "
                     + product,
                     (10, 40),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     1,
-                    (255, 0, 0),
+                    (255, 255, 255),
                     2)
+
+        cv2.putText(frame,
+                    resp.json()["product"]["product_name"],
+                    (10, 80),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2)
+
+        pprint(resp.json())
+
         cap.release()
         cv2.imshow("result", frame)
         cv2.waitKey(0)
@@ -46,5 +58,5 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cv2.release()
+cap.release()
 cv2.destroyAllWindows()
