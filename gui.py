814bfdd7c5c11ee1ce0cf2cba3678665c24eaff0
omni <felixgif@gmail.com>
2018-06-17 00:16:11 +0200

    update gui

--- gui.py
+++ gui.py
@@ -24,7 +24,11 @@ class GuiApp:
         self.frameMenu.grid(row=0, column=0)
         self.framePres = tk.Frame(self.root)
         self.framePres.grid(row=0, column=1)
-
+        self.framePresLeft = tk.Frame(self.framePres)
+        self.framePresLeft.grid(row=0, column=0)
+        self.framePresRight = tk.Frame(self.framePres)
+        self.framePresRight.grid(row=0, column=1)
+        
         self.listbox = tk.Listbox(self.frameMenu)
         self.listbox.pack()
 
@@ -54,13 +58,13 @@ class GuiApp:
 
                 activeItem = self.listbox.get(tk.ACTIVE)
                 if activeItem != str():
-                    self.listeWidgets[int(activeItem[0]) - 1][0].grid(row=0, column=0)
-                    self.listeWidgets[int(activeItem[0]) - 1][1].grid(row=0, column=1)
-                    self.listeWidgets[int(activeItem[0]) - 1][2].grid(row=1, column=1)
+                    self.listeWidgets[int(activeItem[0]) - 1][0].pack()
+                    self.listeWidgets[int(activeItem[0]) - 1][1].pack()
+                    self.listeWidgets[int(activeItem[0]) - 1][2].pack()
                     for elt in [i for i in self.listeWidgets if i != self.listeWidgets[int(activeItem[0]) - 1]]:
-                        elt[0].grid_forget()
-                        elt[1].grid_forget()
-                        elt[2].grid_forget()
+                        elt[0].pack_forget()
+                        elt[1].pack_forget()
+                        elt[2].pack_forget()
         except RuntimeError:
             print("[INFO] caught a RuntimeError")
 
@@ -73,14 +77,14 @@ class GuiApp:
     def add_item(self, data):
         self.listeProduct.append(data)
         resp = requests.get("https://fr.openfoodfacts.org/api/v0/produit/" + data + ".json")
-        imageUrl = requests.get(resp.json()["product"]["image_front_small_url"])
+        imageUrl = requests.get(resp.json()["product"]["image_url"])
         img = ImageTk.PhotoImage(Image.open(BytesIO(imageUrl.content)))
-        presIcon = tk.Label(self.framePres, image=img)
-        presIcon.grid_forget()
-        presBrand = tk.Label(self.framePres, text = resp.json()["product"]["brands"])
-        presBrand.grid_forget()
+        presIcon = tk.Label(self.framePresLeft, image=img)
+        presIcon.pack_forget()
+        presBrand = tk.Label(self.framePresRight, text = resp.json()["product"]["brands"])
+        presBrand.pack_forget()
         productName = resp.json()["product"]["product_name"]
-        presProduct = tk.Label(self.framePres, text = productName)
-        presProduct.grid_forget()
+        presProduct = tk.Label(self.framePresRight, text = productName)
+        presProduct.pack_forget()
         self.listeWidgets.append([presIcon, presBrand, presProduct])
         self.listbox.insert(tk.END, str(self.listeProduct.__len__()) + " " + productName)
