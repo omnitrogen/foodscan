2624d25e4e2f3c9eca0543f212662653c7b96e04
omni <felixgif@gmail.com>
2018-06-19 00:37:14 +0200

    update gui

--- gui.py
+++ gui.py
@@ -133,6 +133,9 @@ class GuiApp:
             table += "<tbody><tr><td>" + elt[0] + "</td><td>" + elt[1] + "</td></tr></tbody>"
 
         outputFile = "output" + str(int(time.time()))
+        print("1", htmlPage)
+        print("2", htmlPage.format(table=table))
+        print("3", htmlPage.format(table=table).splitlines())
         with open(outputFile, "w") as f:
-            for elt in htmlPage.format(table=table).lstrip().splitlines(): 
+            for elt in htmlPage.format(table=table).splitlines():
                 f.write(elt)
